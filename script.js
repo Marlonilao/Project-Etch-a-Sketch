622da2a73a368d3a53fa64a95dc445de89b9a79d
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-03-05 19:45:40 +0800

    Finalize project

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
-const container = document.querySelector('.container');
+const drawingBoard = document.querySelector('.drawingBoard');
 
 for (let i=0; i < 16; i++) {
     const div = document.createElement('div');
-    container.appendChild(div);
+    div.className = 'out';
+    drawingBoard.appendChild(div);
     for (let i=0; i < 16; i++) {
-        const divv = document.createElement('div');
-        divv.className = 'box';
-        div.appendChild(divv);
-    }
-}
+        const box = document.createElement('div');
+        box.className = 'box';
+        div.appendChild(box);
+    };
+};
 
-const box = document.querySelectorAll('.box');
-box.forEach(box => box.addEventListener('mousemove' , (e) => {
-    e.target.style.backgroundColor = 'pink';
-}))
+
+document.addEventListener('DOMContentLoaded',(e) => {
+    let isDrawing = false
+
+    document.addEventListener('mousedown' , (e) => {
+        if (e.button === 0) {
+            isDrawing = true;
+        }
+    });
+
+    document.addEventListener('mouseup', (e) => {
+        isDrawing = false;
+    });
+
+    const box = document.querySelectorAll('.box');
+    box.forEach(box => box.addEventListener('mousemove' , (e) => {
+        if (isDrawing == true) {
+            e.target.style.backgroundColor = 'gray';
+        }
+    }));
+
+});
+
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    const numberOfSquares = prompt('Enter a number for squares per side for the new grid: ');
+    if (numberOfSquares > 100) {
+        alert('Please enter a number less than or equal to 100');
+    } else {
+    drawingBoard.innerHTML = '<div></div>'
+    for (let i=0; i < numberOfSquares; i++) {
+        const div = document.createElement('div');
+        div.className = 'out';
+        drawingBoard.appendChild(div);
+        for (let i=0; i < numberOfSquares; i++) {
+            const box = document.createElement('div');
+            box.className = 'box';
+            div.appendChild(box);
+        }
+    }
+    let isDrawing = false;
+    document.addEventListener('mousedown' , (e) => {
+        if (e.button === 0) {
+            isDrawing = true;
+        }
+    });
+
+    document.addEventListener('mouseup', (e) => {
+        isDrawing = false;
+    });
+
+    const box = document.querySelectorAll('.box');
+    box.forEach(box => box.addEventListener('mousemove' , (e) => {
+        if (isDrawing == true) {
+            e.target.style.backgroundColor = 'gray';
+        }
+    }))};
+});
